2bb17faebfe8c933a7232d94aae2b32f05b39f23
Strzempek <martyna.strzempek@atos.net>
2019-08-20 17:17:59 +0200

    Add tests

--- test/memoize.spec.js
+++ test/memoize.spec.js
@@ -34,4 +34,23 @@ describe('Memoize decorator', () => {
         assert.equal(spy.called, true);
         sinon.assert.calledOnce(spy);
       });
+
+      it('should only call wrapped function once if called with the same parameters multiply times', () => {
+        const square = (n) => n**2;
+        const spy = sinon.spy(square);
+        const memoizedSquare = memoize(spy);
+
+        sinon.assert.notCalled(spy);
+
+        const result1 = memoizedSquare(5);
+        const result2 = memoizedSquare(5);
+        const result3 = memoizedSquare(5);
+
+        sinon.assert.calledOnce(spy);
+        sinon.assert.alwaysCalledWithExactly(spy, 5);
+
+        result1.should.equal(25);
+        result2.should.equal(25);
+        result3.should.equal(25);
+      });
   });
