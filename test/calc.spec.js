fd06587b50a78a647196624e9604876d6e733c72
Strzempek <martyna.strzempek@atos.net>
2019-08-20 13:44:52 +0200

    first commit

---
+++ test/calc.spec.js
@@ -0,0 +1,13 @@
+const { Calculator } = require('../src/calc');
+const assert = require('assert');
+
+
+
+describe('Calculator', () => {
+
+  it('should add numbers', () => {
+
+    assert.equal(Calculator.add(2, 2), 4);
+  });
+});
+
